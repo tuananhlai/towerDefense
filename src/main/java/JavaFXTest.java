bcaa84ef4ef0665378278495709bc25b65ec641a
BryantShepherd <42168740+BryantShepherd@users.noreply.github.com>
2019-11-01 19:36:35 +0700

    add javafx 13 sdk

--- src/main/java/JavaFXTest.java
+++ src/main/java/JavaFXTest.java
@@ -1,34 +1,26 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.scene.Group;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class JavaFXTest extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Path path = new Path();
-        MoveTo moveTo = new MoveTo(108, 71);
-        LineTo line1 = new LineTo(321, 161);
-        LineTo line2 = new LineTo(126, 232);
-        LineTo line3 = new LineTo(232, 52);
-        LineTo line4 = new LineTo(269, 250);
-        LineTo line5 = new LineTo(108, 71);
+public class JavaFXTest extends Application  {
 
-        path.getElements().add(moveTo);
-        path.getElements().addAll(line1, line2, line3, line4, line5);
-        Group root = new Group(path);
-        Scene scene = new Scene(root, 600, 800);
-        primaryStage.setTitle("Drawing");
-        primaryStage.setScene(scene);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("HBox Experiment 1");
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(new Button("hello")));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
---
+++ src/readme.txt
@@ -0,0 +1,2 @@
+1. Viết hàm nào thì comment kiểu javadoc nhé.
+2. M đặt tên hàm và biến bằng tiếng Anh kiểu camelCase nhé.
